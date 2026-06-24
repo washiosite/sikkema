3489210209e6a62289c6643d0bd5ecf10e81c84a
washios <aigorudogyarakushi2@gmail.com>
2026-06-24 16:40:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const n = document.getElementById("n");
+const table = document.getElementById("table");
 
-function generate() {
-    n.textContent = Math.floor(Math.random() * 10);
+let count = Array(10).fill(0);
+
+for(let i=0;i<10;i++){
+    table.innerHTML += `<tr><td>${i}</td><td id="c${i}">0</td></tr>`;
 }
 
+function generate(){
+    const x = Math.floor(Math.random()*10);
+    n.textContent = x;
 
-document.addEventListener("keydown", e => {
-    if (e.key.toLowerCase() === "q") {
-        generate();
+    count[x]++;
+    document.getElementById(`c${x}`).textContent = count[x];
+}
+
+document.addEventListener("keydown",e=>{
+    switch(e.key.toLowerCase()){
+        case "q":
+            generate();
+            break;
+
+        case "r":
+            count.fill(0);
+            for(let i=0;i<10;i++){
+                document.getElementById(`c${i}`).textContent=0;
+            }
+            break;
     }
 });
 
