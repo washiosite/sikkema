49364623189159e07630569be27d719ab7891b87
washios <aigorudogyarakushi2@gmail.com>
2026-06-24 16:36:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function generate() {
     n.textContent = Math.floor(Math.random() * 10);
 }
 
+
 document.addEventListener("keydown", e => {
     if (e.key.toLowerCase() === "q") {
         generate();
